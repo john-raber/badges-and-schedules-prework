c6090fce575760bfe3cc5fb1cc601fe525a41ca4
John Raber <jrab224@gmail.com>
2018-06-02 17:59:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  batch_badge_creator(array).each do |message|
+    puts message
+  end
+  assign_rooms(array).each do |room|
+    puts room
+  end
 end
