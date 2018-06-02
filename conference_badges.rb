eadd5c6d498805d8f403f585c895540b662f8aa0
John Raber <jrab224@gmail.com>
2018-06-02 17:54:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ def assign_rooms(array)
   room_assignments = []
   array.each_with_index { |name, index|
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
-  end
+  }
   room_assignments
 end
